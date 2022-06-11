7f74386352fab7691e18269e0fd54c626edc88aa
Woodie Westbrook <wwestbrook@bluestarretirement.com>
2022-06-10 23:39:58 -0400

    initial

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <NavBar />
+  <div class="container is-max-desktop px-2 py-4">
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+import NavBar from './components/layout/NavBar.vue';
+</script>
+
+<style>
+@import 'bulma/css/bulma.min.css';
+</style>
